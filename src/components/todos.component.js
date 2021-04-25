1c2afcfca359af4c010daaf12adb337de58532a6
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-04-25 20:42:00 +0800

    converted to hooks, service pattern, added delete functionality

--- src/components/todos.component.js
+++ src/components/todos.component.js
@@ -1,59 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-class Todos extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [],
-            input: ''
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+const inMemoryTodoService = () => {
+    const todos = new Map();
+    return {
+        load: () => Array.from(todos.values()) ?? [],
+        create: (todo) => {
+            todos.set(todo.id, todo);
+        },
+        updateDone: (id, done) => {
+            const todo = todos.get(id);
+            todo.done = done;
+            todos.set(id, todo);
+        },
+        deleteDone: () => {
+            todos.forEach((todo, id, todos) => todo.done && todos.delete(id));
+        }
     }
+}
 
-    handleChange(event) {
-        this.setState({
-            input: event.target.value
-        });
+const localStorageTodoService = () => ({
+    load: () => {
+        const todos = JSON.parse(localStorage.getItem('todos')) ?? {};
+        return Array.from(Object.values(todos));
+    },
+    create: (todo) => {
+        const todos = JSON.parse(localStorage.getItem('todos')) ?? {};
+        const id = Object.keys(todos).length;
+        todo.id = id;
+        localStorage.setItem('todos', JSON.stringify({ ...todos, [id]: todo }));
+    },
+    updateDone: (id, done) => {
+        const todos = JSON.parse(localStorage.getItem('todos'));
+        const todo = todos[id];
+        if (!todo) return;
+        todo.done = done;
+        localStorage.setItem('todos', JSON.stringify(todos));
+    },
+    deleteDone: () => {
+        const todos = JSON.parse(localStorage.getItem('todos')) ?? {};
+        for (var id in todos) {
+            if (todos[id].done) {
+                delete todos[id];
+            }
+        }
+        localStorage.setItem('todos', JSON.stringify(todos));
     }
+})
 
-    handleSubmit(event) {
+const service = inMemoryTodoService();
+//const service = localStorageTodoService();
+
+var nextid = 0;
+
+const Todos = () =>  {
+
+    const [input, setInput] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const fetchedTodos = service.load();
+        setTodos(fetchedTodos);
+    }, [])
+
+    const handleChange = event => setInput(event.target.value);
+
+    const handleSubmit = (event) => {
         event.preventDefault();
-        this.setState({
-            todos: [...this.state.todos, this.state.input],
-            input: ''
-        });
+        var newTodo = {
+            id: nextid,
+            text: input,
+            done: false
+        };
+        nextid++;
+        service.create(newTodo);
+        setTodos([...todos, newTodo]);
+        setInput('');
+    };
+
+    const handleChecked = (id, done) => {
+        service.updateDone(id, !done);
+        const fetchedTodos = service.load();
+        setTodos(fetchedTodos);
     }
 
-    render() {
-        return(
-            <div>
-                <h1>Add Todo</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <input value={this.state.input} onChange={this.handleChange}/>
-                    <br/>
-                    <button type='submit'>Submit</button>
-                </form>
-                <h1 id="todo-header">Todo List</h1>
-                <Checklist items={this.state.todos}/>
-            </div>
-        );
+    const handleDeleteDone = () => {
+        service.deleteDone();
+        const fetchedTodos = service.load();
+        setTodos(fetchedTodos);
     }
-};
 
-
-const Checklist = (props) => {
-    return (
-        <ul>
-            {props.items.map((item) => (
-                <li key={item}>
-                    <label><input type='checkbox' value={item}/>{item}</label>
-                </li>
-            ))}
-        </ul>
+    return(
+        <div>
+            <h1>Add Todo</h1>
+            <form onSubmit={handleSubmit}>
+                <input value={input} onChange={handleChange}/>
+                <br/>
+                <button type='submit'>Submit</button>
+            </form>
+            <h1 id="todo-header">Todo List</h1>
+            <Checklist todos={todos} onCheck={handleChecked}/>
+            <button type="button" onClick={handleDeleteDone}>Delete Done Todos</button>
+        </div>
     );
-};
+}
+
+const Checklist = ({todos, onCheck}) => 
+    <ul>
+        {todos.map(({id, text, done}) => (
+            <li key={id}>
+                <label className={done ? "done-todo" : ""}><input type='checkbox' checked={done} onChange={() => onCheck(id, done)}/>{text}</label>
+            </li>
+        ))}
+    </ul>
 
 export default Todos;
