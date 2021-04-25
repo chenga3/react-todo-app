0448550f1ef6d892921e0fd0d8c8697d09382d6a
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-04-25 20:46:50 +0800

    moved todos components to pages folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import Todos from './components/todos.component';
+import Todos from "./pages/todos";
 
 function App() {
   return (
-    <div className='app'>
-      <Todos/>
+    <div className="app">
+      <Todos />
     </div>
   );
 }
