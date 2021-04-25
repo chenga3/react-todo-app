389ce69d7d6ebdcbd68a0955abb7c55c0d7d0f62
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-04-25 21:20:56 +0800

    added typescript, fixed craco dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import Todos from "./pages/todos";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="p-4">
       <Todos />
     </div>
   );
-}
+};
 
 export default App;
