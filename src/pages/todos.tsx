a6484b24aa7511863e8b159670166d66417d65bf
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-04-25 22:16:54 +0800

    converted todo page to tsx

--- src/pages/todos.tsx
+++ src/pages/todos.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
+import type { Todo } from "../types";
 
 const inMemoryTodoService = () => {
   const todos = new Map();
   return {
     load: () => Array.from(todos.values()) ?? [],
-    create: (todo) => {
+    create: (todo: Todo) => {
       todos.set(todo.id, todo);
     },
-    updateDone: (id, done) => {
+    updateDone: (id: number, done: boolean) => {
       const todo = todos.get(id);
       todo.done = done;
       todos.set(id, todo);
@@ -20,24 +21,40 @@ const inMemoryTodoService = () => {
 
 const localStorageTodoService = () => ({
   load: () => {
-    const todos = JSON.parse(localStorage.getItem("todos")) ?? {};
+    const storageItem = localStorage.getItem("todos");
+    if (!storageItem) {
+      return {};
+    }
+    const todos = JSON.parse(storageItem);
     return Array.from(Object.values(todos));
   },
-  create: (todo) => {
-    const todos = JSON.parse(localStorage.getItem("todos")) ?? {};
+  create: (todo: Todo) => {
+    const storageItem = localStorage.getItem("todos");
+    if (!storageItem) {
+      return;
+    }
+    const todos = JSON.parse(storageItem);
     const id = Object.keys(todos).length;
     todo.id = id;
     localStorage.setItem("todos", JSON.stringify({ ...todos, [id]: todo }));
   },
-  updateDone: (id, done) => {
-    const todos = JSON.parse(localStorage.getItem("todos"));
+  updateDone: (id: number, done: boolean) => {
+    const storageItem = localStorage.getItem("todos");
+    if (!storageItem) {
+      return;
+    }
+    const todos = JSON.parse(storageItem);
     const todo = todos[id];
     if (!todo) return;
     todo.done = done;
     localStorage.setItem("todos", JSON.stringify(todos));
   },
   deleteDone: () => {
-    const todos = JSON.parse(localStorage.getItem("todos")) ?? {};
+    const storageItem = localStorage.getItem("todos");
+    if (!storageItem) {
+      return;
+    }
+    const todos = JSON.parse(storageItem);
     for (var id in todos) {
       if (todos[id].done) {
         delete todos[id];
@@ -53,17 +70,18 @@ const service = inMemoryTodoService();
 var nextid = 0;
 
 const Todos = () => {
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState<string>("");
+  const [todos, setTodos] = useState<Array<Todo>>([]);
 
   useEffect(() => {
     const fetchedTodos = service.load();
     setTodos(fetchedTodos);
   }, []);
 
-  const handleChange = (event) => setInput(event.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setInput(event.target.value);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     var newTodo = {
       id: nextid,
@@ -76,7 +94,7 @@ const Todos = () => {
     setInput("");
   };
 
-  const handleChecked = (id, done) => {
+  const handleChecked = (id: number, done: boolean) => {
     service.updateDone(id, !done);
     const fetchedTodos = service.load();
     setTodos(fetchedTodos);
@@ -84,7 +102,7 @@ const Todos = () => {
 
   const handleDeleteDone = () => {
     service.deleteDone();
-    const fetchedTodos = service.load();
+    const fetchedTodos: Array<Todo> = service.load();
     setTodos(fetchedTodos);
   };
 
@@ -111,7 +129,15 @@ const Todos = () => {
   );
 };
 
-const Checklist = ({ todos, onCheck }) => (
+interface ChecklistProps {
+  todos: Array<Todo>;
+  onCheck: (id: number, done: boolean) => void;
+}
+
+const Checklist: React.FC<ChecklistProps> = ({
+  todos,
+  onCheck,
+}: ChecklistProps) => (
   <ul>
     {todos.map(({ id, text, done }) => (
       <li key={id}>
