2d544d27f82e8eef6507654b392084aa34e20a1d
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-06-17 14:15:49 +0800

    added basic timer display, fixed timer context

--- src/timer/Timer.tsx
+++ src/timer/Timer.tsx
@@ -4,7 +4,17 @@ import { TimerContext } from "../contexts/timer-context";
 
 const Timer: React.FC = () => {
   return (
-    <TimerContext.Consumer>{(timer) => <div></div>}</TimerContext.Consumer>
+    <TimerContext.Consumer>
+      {(timer) => (
+        <div>
+          <h2>Work Timer</h2>
+          {timer.workTimer.timeLeft.join(":")}
+          <br />
+          <h2>Break Timer</h2>
+          {timer.breakTimer.timeLeft.join(":")}
+        </div>
+      )}
+    </TimerContext.Consumer>
   );
 };
 
