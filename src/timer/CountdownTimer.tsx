d38ca57a0a20225c9f192194b64438612fda780f
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-06-18 13:36:09 +0800

    added status state, working start button

--- src/timer/CountdownTimer.tsx
+++ src/timer/CountdownTimer.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 
 const CountdownTimer: React.FC = () => {
+  const [status, setStatus] = useState<string>("OFF");
   const [timeLeft, setTimeLeft] = useState<Array<number>>([0, 0, 0]);
   const [startTime, setStartTime] = useState<Array<number>>([0, 0, 10]);
+
+  const startTimer = () => {
+    setTimeLeft([...startTime]);
+    setStatus("ON");
+  };
+
   return (
     <div>
+      <div>Timer is {status}</div>
       <div>{timeLeft.join(":")}</div>
       <div>{startTime.join(":")}</div>
-      <button>Start</button>
+      <button onClick={startTimer}>Start</button>
     </div>
   );
 };
