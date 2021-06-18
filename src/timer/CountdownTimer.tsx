3edfdc84b078822240343fa3239224870ffa2bdf
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-06-18 14:01:25 +0800

    added checkDone method, finished status

--- src/timer/CountdownTimer.tsx
+++ src/timer/CountdownTimer.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 const CountdownTimer: React.FC = () => {
   const [status, setStatus] = useState<string>("OFF");
   const [timeLeft, setTimeLeft] = useState<Array<number>>([0, 0, 0]);
-  const [startTime, setStartTime] = useState<Array<number>>([0, 0, 10]);
+  const [startTime, setStartTime] = useState<Array<number>>([0, 0, 3]);
 
   const startTimer = () => {
     setTimeLeft([...startTime]);
@@ -24,13 +24,20 @@ const CountdownTimer: React.FC = () => {
       setTimeLeft([timeLeft[0], timeLeft[1], timeLeft[2] - 1]);
     };
 
+    const checkDone = () => {
+      return timeLeft[0] === 0 && timeLeft[1] === 0 && timeLeft[2] === 0;
+    };
+
     const timer = setInterval(() => {
       if (status === "ON") {
         countdown();
       }
     }, 1000);
+    if (checkDone()) {
+      setStatus("FINISHED");
+    }
     return () => clearTimeout(timer);
-  });
+  }, [status, timeLeft]);
 
   return (
     <div>
