a04e2e3f4eabff555e128b9e0ed37fdb72254e45
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-06-18 13:33:17 +0800

    added states for time left and start time

--- src/timer/CountdownTimer.tsx
+++ src/timer/CountdownTimer.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CountdownTimer: React.FC = () => {
-  return <div>CountdownTimer</div>;
+  const [timeLeft, setTimeLeft] = useState<Array<number>>([0, 0, 0]);
+  const [startTime, setStartTime] = useState<Array<number>>([0, 0, 10]);
+  return (
+    <div>
+      <div>{timeLeft.join(":")}</div>
+      <div>{startTime.join(":")}</div>
+      <button>Start</button>
+    </div>
+  );
 };
 
 export default CountdownTimer;
