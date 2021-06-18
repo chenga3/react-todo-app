08d476d14d9dbccdacabf41b30683bdc3cbee7be
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-06-18 13:11:17 +0800

    purged unusable timer code

--- src/App.tsx
+++ src/App.tsx
@@ -1,152 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import { TimerContext } from "./contexts/timer-context";
-import { PomodoroTimer, Timer } from "./types";
 import Todos from "./pages/Todos";
 import Pomodoro from "./pages/Pomodoro";
 import Navbar from "./core/Navbar";
 
 const App: React.FC = () => {
-  const [cycle, setCycle] = useState<string>("Off");
-  const [workTimerOn, setWorkTimerOn] = useState<boolean>(false);
-  const [breakTimerOn, setBreakTimerOn] = useState<boolean>(false);
-  const [pomodoroTimerOn, setPomodoroTimerOn] = useState<boolean>(false);
-  const [workTimeLeft, setWorkTimeLeft] = useState<Array<number>>([0, 0, 10]);
-  const [breakTimeLeft, setBreakTimeLeft] = useState<Array<number>>([0, 0, 5]);
-  const [workTime, setWorkTime] = useState<Array<number>>([0, 0, 10]);
-  const [breakTime, setBreakTime] = useState<Array<number>>([0, 0, 5]);
-  const [sound, setSound] = useState<boolean>(true);
-
-  const workTimer: Timer = {
-    on: workTimerOn,
-    timeLeft: workTimeLeft,
-    startTime: workTime,
-    sound: sound,
-    setStartTime: setWorkTime,
-    setSound: setSound,
-    countdown: () => {
-      if (
-        workTimeLeft[0] === 0 &&
-        workTimeLeft[1] === 0 &&
-        workTimeLeft[2] === 0
-      ) {
-        workTimer.reset();
-      } else if (workTimeLeft[1] === 0 && workTimeLeft[2] === 0) {
-        setWorkTimeLeft([workTimeLeft[0] - 1, 59, 59]);
-      } else if (workTimeLeft[2] === 0) {
-        setWorkTimeLeft([workTimeLeft[0], workTimeLeft[1] - 1, 59]);
-      } else {
-        setWorkTimeLeft([
-          workTimeLeft[0],
-          workTimeLeft[1],
-          workTimeLeft[2] - 1,
-        ]);
-      }
-    },
-    reset: () => {
-      setWorkTimeLeft([...workTimer.startTime]);
-      setWorkTimerOn(false);
-    },
-  };
-
-  const breakTimer: Timer = {
-    on: breakTimerOn,
-    timeLeft: breakTimeLeft,
-    startTime: breakTime,
-    sound: sound,
-    setStartTime: setBreakTime,
-    setSound: setSound,
-    countdown: () => {
-      if (
-        breakTimeLeft[0] === 0 &&
-        breakTimeLeft[1] === 0 &&
-        breakTimeLeft[2] === 0
-      ) {
-        breakTimer.reset();
-      } else if (breakTimeLeft[1] === 0 && breakTimeLeft[2] === 0) {
-        setBreakTimeLeft([breakTimeLeft[0] - 1, 59, 59]);
-      } else if (breakTimeLeft[2] === 0) {
-        setBreakTimeLeft([breakTimeLeft[0], breakTimeLeft[1] - 1, 59]);
-      } else {
-        setBreakTimeLeft([
-          breakTimeLeft[0],
-          breakTimeLeft[1],
-          breakTimeLeft[2] - 1,
-        ]);
-      }
-    },
-    reset: () => {
-      setBreakTimeLeft([...breakTimer.startTime]);
-      setBreakTimerOn(false);
-    },
-  };
-
-  const pomodoroTimer: PomodoroTimer = {
-    on: pomodoroTimerOn,
-    cycle: cycle,
-    workTimer: workTimer,
-    breakTimer: breakTimer,
-    countdown: () => {
-      if (cycle === "Work") {
-        workTimer.countdown();
-        if (!workTimer.on) {
-          // Reached end of the work cycle
-          setCycle("Break");
-          setBreakTimerOn(true);
-          breakTimer.countdown();
-        }
-      } else if (cycle === "Break") {
-        breakTimer.countdown();
-        if (!breakTimer.on) {
-          // Reached end of the break cycle
-          setCycle("Work");
-          setWorkTimerOn(true);
-          workTimer.countdown();
-        }
-      }
-    },
-    start: () => {
-      setPomodoroTimerOn(true);
-      setCycle("Work"); // Start with work cycle
-      setWorkTimerOn(true);
-      setBreakTimerOn(false);
-    },
-    pause: () => {
-      setPomodoroTimerOn(false);
-    },
-    reset: () => {
-      setPomodoroTimerOn(false);
-      setCycle("Work");
-    },
-  };
-
-  useEffect(() => {
-    pomodoroTimer.start();
-    console.log(workTimer.on);
-  }, []);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      pomodoroTimer.countdown();
-    }, 1000);
-    return () => clearTimeout(timer);
-  });
-
   return (
-    <TimerContext.Provider value={pomodoroTimer}>
-      <div className="p-4">
-        <Navbar />
-        <Switch>
-          <Route path="/todos">
-            <Todos />
-          </Route>
-          <Route path="/pomodoro">
-            <Pomodoro />
-          </Route>
-        </Switch>
-      </div>
-    </TimerContext.Provider>
+    <div className="p-4">
+      <Navbar />
+      <Switch>
+        <Route path="/todos">
+          <Todos />
+        </Route>
+        <Route path="/pomodoro">
+          <Pomodoro />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 
--- src/timer/Timer.tsx
+++ src/timer/Timer.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 
-import { TimerContext } from "../contexts/timer-context";
-
 const Timer: React.FC = () => {
   return (
-    <TimerContext.Consumer>
-      {(timer) => (
-        <div>
-          <h2>Work Timer</h2>
-          {timer.workTimer.timeLeft.join(":")}
-          <br />
-          <h2>Break Timer</h2>
-          {timer.breakTimer.timeLeft.join(":")}
-        </div>
-      )}
-    </TimerContext.Consumer>
+    <div>
+      <h2>Work Timer</h2>
+      <br />
+      <h2>Break Timer</h2>
+    </div>
   );
 };
 
