f63aad3d614e3df31e4038c19bcca4584d890ccd
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-06-29 10:25:04 +0800

    improved express code

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,15 +1,16 @@
 const express = require("express");
-
 const todoRoutes = express.Router();
 
 const dbo = require("../db/conn");
-
 const ObjectId = require("mongodb").ObjectId;
 
+const dbName = "producktivity";
+const colName = "todos";
+
 todoRoutes.route("/todo").get(function (req, res) {
-  let db_connect = dbo.getDb("producktivity");
+  let db_connect = dbo.getDb(dbName);
   db_connect
-    .collection("todos")
+    .collection(colName)
     .find({})
     .toArray(function (err, result) {
       if (err) throw err;
@@ -17,19 +18,19 @@ todoRoutes.route("/todo").get(function (req, res) {
     });
 });
 
-todoRoutes.route("/todo/add").post(function (req, res) {
-  let db_connect = dbo.getDb("producktivity");
+todoRoutes.route("/todo").post(function (req, res) {
+  let db_connect = dbo.getDb(dbName);
   let myobj = {
     text: req.body.text,
     done: req.body.done,
   };
-  db_connect.collection("todos").insertOne(myobj, function (err, res) {
+  db_connect.collection(colName).insertOne(myobj, function (err, res) {
     if (err) throw err;
   });
 });
 
 todoRoutes.route("/todo/:id").post(function (req, res) {
-  let db_connect = dbo.getDb("producktivity");
+  let db_connect = dbo.getDb(dbName);
   let myquery = { _id: ObjectId(req.params.id) };
   let newvalues = {
     $set: {
@@ -38,7 +39,7 @@ todoRoutes.route("/todo/:id").post(function (req, res) {
     },
   };
   db_connect
-    .collection("todos")
+    .collection(colName)
     .updateOne(myquery, newvalues, function (err, res) {
       if (err) throw err;
       console.log(res);
@@ -47,11 +48,11 @@ todoRoutes.route("/todo/:id").post(function (req, res) {
 
 // This section will help you delete a record
 todoRoutes.route("todo/:id").delete((req, res) => {
-  let db_connect = dbo.getDb("producktivity");
-  var myquery = { id: req.body.id };
-  db_connect.collection("todos").deleteOne(myquery, function (err, obj) {
+  let db_connect = dbo.getDb(dbName);
+  var myquery = { id: ObjectId(req.params.id) };
+  db_connect.collection(colName).deleteOne(myquery, function (err, obj) {
     if (err) throw err;
-    console.log("1 document deleted");
+    console.log(res);
   });
 });
 
