3ce216bba33ab9c406add1a76a61054ecfbfc804
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-07-04 17:06:04 +0800

    functional controllers

--- client/src/timer/TimerDisplay.tsx
+++ client/src/timer/TimerDisplay.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Controller from "./Controller";
 import Digit from "./Digit";
 
 interface Props {
@@ -14,6 +15,38 @@ const TimerDisplay: React.FC<Props> = ({
   startTime,
   timeLeft,
 }) => {
+  const addTen = (i: number) => {
+    if (startTime[i] < 50) {
+      var newTime = [...startTime];
+      newTime[i] += 10;
+      setStartTime(newTime);
+    }
+  };
+
+  const removeTen = (i: number) => {
+    if (startTime[i] >= 10) {
+      var newTime = [...startTime];
+      newTime[i] -= 10;
+      setStartTime(newTime);
+    }
+  };
+
+  const addOne = (i: number) => {
+    if (startTime[i] % 10 < 9) {
+      var newTime = [...startTime];
+      newTime[i] += 1;
+      setStartTime(newTime);
+    }
+  };
+
+  const removeOne = (i: number) => {
+    if (startTime[i] % 10 > 0) {
+      var newTime = [...startTime];
+      newTime[i] -= 1;
+      setStartTime(newTime);
+    }
+  };
+
   if (on) {
     return (
       <div className="flex flex-row text-7xl">
@@ -29,29 +62,39 @@ const TimerDisplay: React.FC<Props> = ({
     );
   } else {
     return (
-      <div>
-        <div className="flex flex-row text-7xl">
-          <div>
-            <Digit digit={Math.floor(startTime[0] / 10)} />
-          </div>
-          <div>
-            <Digit digit={startTime[0] % 10} />
-          </div>
-          <div>:</div>
-          <div>
-            <Digit digit={Math.floor(startTime[1] / 10)} />
-          </div>
-          <div>
-            <Digit digit={startTime[1] % 10} />
-          </div>
-          <div>:</div>
-          <div>
-            <Digit digit={Math.floor(startTime[2] / 10)} />
-          </div>
-          <div>
-            <Digit digit={startTime[2] % 10} />
-          </div>
-        </div>
+      <div className="flex flex-row place-items-center text-7xl">
+        <Controller
+          digit={Math.floor(startTime[0] / 10)}
+          addTime={() => addTen(0)}
+          removeTime={() => removeTen(0)}
+        />
+        <Controller
+          digit={startTime[0] % 10}
+          addTime={() => addOne(0)}
+          removeTime={() => removeOne(0)}
+        />
+        <div>:</div>
+        <Controller
+          digit={Math.floor(startTime[1] / 10)}
+          addTime={() => addTen(1)}
+          removeTime={() => removeTen(1)}
+        />
+        <Controller
+          digit={startTime[1] % 10}
+          addTime={() => addOne(1)}
+          removeTime={() => removeOne(1)}
+        />
+        <div>:</div>
+        <Controller
+          digit={Math.floor(startTime[2] / 10)}
+          addTime={() => addTen(2)}
+          removeTime={() => removeTen(2)}
+        />
+        <Controller
+          digit={startTime[2] % 10}
+          addTime={() => addOne(2)}
+          removeTime={() => removeOne(2)}
+        />
       </div>
     );
   }
